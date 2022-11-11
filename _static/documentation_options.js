708a045289345805b974fafb89b76bcdab6af92c
anqi.luo <anqi.luo@ginlong.com>
2022-11-11 16:01:34 +0800

    新test

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: 'v1',
-    LANGUAGE: 'zh_cn',
+    LANGUAGE: 'zh-cn',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
